d779ecdbed0bd32000fef2fdb7fa6dc70b54b582
sally0226 <sally734595@gmail.com>
2021-02-05 18:39:54 +0900

    Add init files
    React 개발환경 구축
    간단한 실습 예제
    props 사용까지 배움.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Subject extends Component {
+  render() { //컴퍼넌트는 반드시 하나의 최상위 태그로 시작해야함 
+    return (
+      <header>
+        <h1>WEB</h1>
+        welcome!
+    </header>
+    );
+  }
 }
+class TOC extends Component{
+  render(){
+    return (
+      <nav>
+        <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">JS</a></li>
+            <li><a href="3.html">CSS</a></li>
+        </ul>
+        </nav>
+    );
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject></Subject>
+        <TOC></TOC>
+      </div>
+    );
+  }
+}
+class HTML extends Component {
 
+}
 export default App;
