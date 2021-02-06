835f1d6c4ba162cefd3aaf5adc6d9047d11d9513
sally0226 <sally734595@gmail.com>
2021-02-06 13:43:06 +0900

    Study event_2
    컴포넌트 이벤트_2
    onClick 함수의 파라미터로 넣어 이용하기

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -11,12 +11,11 @@ class TOC extends Component{
                 <li key={data[i].id}>
                     <a 
                         href={"/content/"+data[i].id}
-                        data-id = {data[i].id}
-                        onClick={function(e){
+                        onClick={function(id,e){
                             e.preventDefault()
                             
-                            this.props.onChangePage(e.target.dataset.id);
-                        }.bind(this)}
+                            this.props.onChangePage(id);
+                        }.bind(this, data[i].id)}
                     >{data[i].title}</a>
                 </li>);
             i = i + 1;
