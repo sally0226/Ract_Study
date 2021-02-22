f544e3855ad340cd1c4a79607e6559ce2767089c
sally0226 <sally734595@gmail.com>
2021-02-22 22:24:10 +0900

    add DateChangeArrow UI
    날짜를 이동할 수 있는 화살표 UI 추가

--- react_todolist/src/App.js
+++ react_todolist/src/App.js
@@ -4,23 +4,37 @@ import TodoHead from './components/TodoHead';
 import TodoList from './components/TodoList';
 import TodoCreate from './components/TodoCreate';
 import {TodoProvider} from './TodoContext';
-
+import TodoChangeDate from './components/TodoChangeDate';
+import styled, {css} from 'styled-components';
 // global style을 추가하고 싶을 때 이용. 
 const GlobalStyle = createGlobalStyle`
-  body {
-    background: #e9ecef;
-  }
+	body {
+		background: #e9ecef;
+	}
+`;
+
+const WrapAll = styled.div`
+	display: flex;
+	width: min-content; //min-content와 max-content의 차이? 
+	margin:0 auto; //페이지 중앙에 오도록 설정
 `;
 
 function App() {
   return (
     <TodoProvider>
-      <GlobalStyle></GlobalStyle>
-      <TodoTemplate>
-        <TodoHead></TodoHead>
-        <TodoList></TodoList>
-        <TodoCreate></TodoCreate>
-      </TodoTemplate>
+		<GlobalStyle></GlobalStyle>
+		<WrapAll>
+			<TodoChangeDate info="left">
+			</TodoChangeDate>
+			<TodoTemplate>
+				<TodoHead></TodoHead>
+				<TodoList></TodoList>
+				<TodoCreate></TodoCreate>
+			</TodoTemplate>
+			<TodoChangeDate info="right">
+			</TodoChangeDate>
+		</WrapAll>
+		
     </TodoProvider>
   );
 }
