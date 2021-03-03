23c8ae931725d06b47b7ae7e9e390b9ddd86892c
sally0226 <sally734595@gmail.com>
2021-03-03 22:32:48 +0900

    Update Header.js
    미완성

--- nodejs_react_todolist/frontend/src/components/Header.js
+++ nodejs_react_todolist/frontend/src/components/Header.js
@@ -4,7 +4,11 @@ import styled from 'styled-components';
 const HeaderBlock = styled.div`
     width: parent;
     height: 100px;
+    disppay: flex;
+    align-items: center;
+    justify-content: space-between;
 
+    
 `;
 
 const LogInBtn = styled.a`
@@ -18,6 +22,12 @@ function Header(){
     return(
         <HeaderBlock>
             Header 입니다.
+            <nav>
+                <ul>
+                    <ll><a href="">log in</a></ll>
+                    <ll><a href="">join in</a></ll>
+                </ul>
+            </nav>
             <LogInBtn href=''>
                 로그인
             </LogInBtn>
